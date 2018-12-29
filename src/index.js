ca24fd8b329f9faf70a744b77d3814c5821c2920
Tirapat <mind1990@gmail.com>
2018-12-29 15:53:19 -0500

    adds App, CommentDetail and ApprovalCard

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className='ui container comments'>
+      <ApprovalCard />
+      <CommentDetail
+        author='Sam'
+        timeAgo='Today at 4:45PM'
+        content='Nice blog post!'
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author='Alex'
+        timeAgo='Today at 2:00PM'
+        content='Cool!'
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author='Jane'
+        timeAgo='Yesterday at 5:00PM'
+        content='Have a nice day!'
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'));
